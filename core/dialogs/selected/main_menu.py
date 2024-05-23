b5458c6aa376287ccef3ca1b10e6e540447ad0ea
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-23 13:11:43 +1000

    create main_menu selected

---
+++ core/dialogs/selected/main_menu.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from aiogram.filters.state import StatesGroup, State
+from aiogram.types import CallbackQuery, Message
+from aiogram_dialog import Dialog, DialogManager
+from aiogram_dialog.widgets.kbd import Button
+
+# from core.dialogs.main_menu import getters
+from core.dialogs.states import all_states
+
+
+async def on_main_menu(
+        callback: CallbackQuery,
+        button: Button,
+        manager: DialogManager,
+):
+    await manager.start(state=all_states.main.start)
+
