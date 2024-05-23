e1f2545b87db8e90f9e5ae5a2380ae99095a6fe0
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-23 13:12:25 +1000

    ...

---
+++ core/dialogs/getters/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    'get_registration_data',
+    'g_user',
+]
+
+from .registration_bot import get_registration_data
+from .users import g_user
---
+++ core/dialogs/selected/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'on_registration_bot',
+    'on_registration_tournament',
+    'on_main_menu',
+]
+
+from .registration_bot import on_registration_bot
+from .registration_tournament import on_registration_tournament
+from .main_menu import on_main_menu
