68cc7b0fa2cbdf53415ca185120e71d85db25494
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-23 17:32:29 +1000

    add keyboard pagination for choice tournament

---
+++ core/dialogs/keyboards.py
@@ -0,0 +1,26 @@
+import operator
+
+from aiogram_dialog.widgets.kbd import Select, ScrollingGroup
+from aiogram_dialog.widgets.text import Format
+
+
+SCROLLING_HEIGHT = 6
+SCROLLING_WIDTH = 3
+
+
+def paginated_tournaments(
+        on_click,
+        width: int = SCROLLING_HEIGHT,
+        height: int = SCROLLING_WIDTH
+):
+    return ScrollingGroup(
+        Select(
+            Format("{pos}. {item.name}"),
+            id="s_scroll_products",
+            item_id_getter=operator.attrgetter('id'),
+            items="tournaments",
+            on_click=on_click,
+        ),
+        id="tournament_ids",
+        width=width, height=height,
+    )
