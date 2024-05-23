c7826bb05661b33899d10a4dffa09f259d7fffc0
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-23 12:58:24 +1000

    create schemas for Users

---
+++ core/dialogs/schemas/users.py
@@ -0,0 +1,36 @@
+from typing import TYPE_CHECKING
+from pydantic import BaseModel, ConfigDict
+from typing import Annotated, Optional
+from annotated_types import MinLen, MaxLen, Ge, Le, Len
+
+
+class UserBase(BaseModel):
+    id_telegram: int
+    first_name: Annotated[str, MinLen(3), MaxLen(50)]
+    last_name: Optional[Annotated[str, MinLen(3), MaxLen(50)]]
+    phone: Annotated[str, MinLen(11), MaxLen(11)]
+    handicap: Annotated[float, Ge(-7.), Le(54.)] = 54.
+    image_src: str = 'No content yet'
+    status: bool = True
+
+
+class CreateUser(UserBase):
+    pass
+
+
+class UpdateUser(UserBase):
+    pass
+
+
+class UpdateUserPartial(UserBase):
+    id_telegram: Optional[int] = None
+    first_name: Optional[Annotated[str, MinLen(3), MaxLen(50)]] = None
+    last_name: Optional[Annotated[str, MinLen(3), MaxLen(50)]] = None
+    handicap: float = None
+    image_src: Optional[str] = None
+    status: bool = None
+
+
+class User(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
