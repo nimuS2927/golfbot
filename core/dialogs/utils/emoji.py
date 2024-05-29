d7b8b1b82742e0aff48d3ea5df8fe8484ca32a42
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-29 12:40:36 +1000

    create emoji

--- core/dialogs/utils/emoji.py
+++ core/dialogs/utils/emoji.py
@@ -1,13 +1,33 @@
+from aiogram_dialog import DialogManager
+
+
+EMOJI_DICT = {
+            2: '🔷',
+            1: '🟦',
+            0: '🟨',
+            -1: '🟧',
+            -2: '🔶',
+            -3: '💠',
+        }
+
+
+async def get_emoji(
+        dialog_manager: DialogManager,
+        **middleware_data
+):
+    emoji = {
+            2: '🔷',
+            1: '🟦',
+            0: '🟨',
+            5: '🟧',
+            4: '🔶',
+            3: '💠',
+        }
+    return {'emoji': emoji}
+
 
 def for_holes(key: int) -> str:
-    data = {
-        2: '🔷',
-        1: '🟦',
-        0: '🟨',
-        -1: '🟧',
-        -2: '🔶',
-        -3: '💠',
-    }
+    data = EMOJI_DICT
     if key < -3:
         return data[-3]
     elif key > 2:
