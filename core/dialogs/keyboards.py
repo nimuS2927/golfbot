f24e3d47ea055b30744d0a6e6c920d5c8718805d
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-29 12:38:06 +1000

    add keyboard alphabet and rename id other keyboards

--- core/dialogs/keyboards.py
+++ core/dialogs/keyboards.py
@@ -16,7 +16,7 @@ def paginated_tournaments(
     return ScrollingGroup(
         Select(
             Format("{pos}. {item.name}"),
-            id="s_scroll_products",
+            id="s_scroll_tournaments",
             item_id_getter=operator.attrgetter('id'),
             items="tournaments",
             on_click=on_click,
@@ -29,16 +29,34 @@ def paginated_tournaments(
 def paginated_holes(
         on_click,
         width: int = SCROLLING_HEIGHT,
-        height: int = SCROLLING_WIDTH
+        height: int = SCROLLING_WIDTH,
 ):
     return ScrollingGroup(
         Select(
             Format("{item[3]} №{item[1]} - ({item[2]})"),
-            id="s_scroll_products",
+            id="s_scroll_holes",
             item_id_getter=operator.itemgetter(0),
             items="holes_list",
             on_click=on_click,
         ),
         id="hole_ids_kb",
+        width=width, height=height,
+    )
+
+
+def paginated_alphabet(
+        on_click,
+        width: int = SCROLLING_HEIGHT,
+        height: int = SCROLLING_WIDTH
+):
+    return ScrollingGroup(
+        Select(
+            Format("{item[1]}"),
+            id="s_scroll_letters",
+            item_id_getter=operator.itemgetter(0),
+            items="alphabet",
+            on_click=on_click,
+        ),
+        id="alphabet_kb",
         width=width, height=height,
     )
